c7d9eeb36e36c1a12901da3bd4c3aaeb803840a4
James Eby <admin@freebytech.com>
2021-06-09 12:00:07 -0500

    Added public api version method.

---
+++ src/tivBudget.Dal/VirtualModels/ApiInfoModel.cs
@@ -0,0 +1,13 @@
+namespace tivBudget.Dal.VirtualModels
+{
+  /// <summary>
+  /// The model representing API information.
+  /// </summary>
+  public class ApiInfoModel
+  {
+    /// <summary>
+    /// The version of the current API.
+    /// </summary>
+    public string ApiVersion { get; set; }
+  }
+}
