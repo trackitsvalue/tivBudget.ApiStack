58a68b74a792a929b0f40589ede488bf2ba0004d
James Eby <admin@freebytech.com>
2021-07-10 00:41:27 -0500

    Attempting pooling settings to see if local debugging timeout issues go away.

--- src/tivBudget.Api/Options/DbOptions.cs
+++ src/tivBudget.Api/Options/DbOptions.cs
@@ -5,33 +5,33 @@ using System.Threading.Tasks;
 
 namespace tivBudget.Api.Options
 {
+  /// <summary>
+  /// Class representing the DB Options environment variables to setup applications DB connection.
+  /// </summary>
+  public class DbOptions
+  {
     /// <summary>
-    /// Class representing the DB Options environment variables to setup applications DB connection.
+    /// The Server Name of the SQL Server to connect to for DB operations, pulled from the environment variable DB__ServerName most likely.
     /// </summary>
-    public class DbOptions
+    public string ServerName { get; set; }
+
+    /// <summary>
+    /// The User Name of the SQL Server to connect to for DB operations, pulled from the environment variable DB__UserName most likely.
+    /// </summary>
+    public string UserName { get; set; }
+
+    /// <summary>
+    /// The User Password of the SQL Server to connect to for DB operations, pulled from the environment variable DB__UserPassword most likely.
+    /// </summary>
+    public string UserPassword { get; set; }
+
+    /// <summary>
+    /// Builds a standard connection string from the environment variables pulled from configuration.
+    /// </summary>
+    /// <returns></returns>
+    public static string BuildConnectionString(string servername, string userName, string userPassword, string initialDb)
     {
-        /// <summary>
-        /// The Server Name of the SQL Server to connect to for DB operations, pulled from the environment variable DB__ServerName most likely.
-        /// </summary>
-        public string ServerName { get; set; }
-
-        /// <summary>
-        /// The User Name of the SQL Server to connect to for DB operations, pulled from the environment variable DB__UserName most likely.
-        /// </summary>
-        public string UserName { get; set; }
-
-        /// <summary>
-        /// The User Password of the SQL Server to connect to for DB operations, pulled from the environment variable DB__UserPassword most likely.
-        /// </summary>
-        public string UserPassword { get; set; }
-
-        /// <summary>
-        /// Builds a standard connection string from the environment variables pulled from configuration.
-        /// </summary>
-        /// <returns></returns>
-        public static string BuildConnectionString(string servername, string userName, string userPassword, string initialDb)
-        {
-            return ($"Data Source={servername};User ID={userName};Password={userPassword};Database={initialDb};Pooling=False;");
-        }
+      return ($"Data Source={servername};User ID={userName};Password={userPassword};Database={initialDb};Pooling=True");
     }
+  }
 }
