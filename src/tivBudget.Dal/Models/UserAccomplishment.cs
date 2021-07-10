fc22b9693db7318357d4655e8e1132563096b380
James Eby <admin@freebytech.com>
2021-07-10 00:43:18 -0500

    Updated Accomplishments and Settings to have non-clustered IDs.

--- src/tivBudget.Dal/Models/UserAccomplishment.cs
+++ src/tivBudget.Dal/Models/UserAccomplishment.cs
@@ -6,15 +6,16 @@ namespace tivBudget.Dal.Models
 {
   public partial class UserAccomplishment : IEditableModel
   {
+    public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public int ApplicationId { get; set; }
     public string Type { get; set; }
     public string SubType { get; set; }
     public Guid AssociatedId { get; set; }
     public int EarnedExperience { get; set; }
+    public string Icon { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Icon { get; set; }
     public bool IsAcknowledged { get; set; }
     public DateTime CreatedOn { get; set; }
     public string CreatedBy { get; set; }
--- src/tivBudget.Db/Security/Tables/UserAccomplishments.sql
+++ src/tivBudget.Db/Security/Tables/UserAccomplishments.sql
@@ -1,19 +1,25 @@
 ﻿CREATE TABLE [Security].[UserAccomplishments] (
-    [UserID]        UNIQUEIDENTIFIER NOT NULL,
+    [ID]            UNIQUEIDENTIFIER CONSTRAINT [DF_UserAccomplishments_ID] DEFAULT (newsequentialid()) NOT NULL,
+	[UserID]        UNIQUEIDENTIFIER NOT NULL,
     [ApplicationID] INT              NOT NULL,
     [Type]			NVARCHAR (50)    NOT NULL,
     [SubType]		NVARCHAR (50)    NOT NULL,
     [AssociatedID]  UNIQUEIDENTIFIER NOT NULL,
+	[EarnedExperience] INT			 NOT NULL,
     [Icon]			NVARCHAR (50)    NOT NULL,
     [Title]         NVARCHAR (128)   NOT NULL,
 	[Description]   NVARCHAR (MAX)   NOT NULL,
-	[IsAcknowledged] BIT             NOT NULL,	
+	[IsAcknowledged] BIT             NOT NULL,
     [CreatedOn]     DATETIME         NOT NULL,
     [CreatedBy]     NVARCHAR (50)    NOT NULL,
     [ModifiedOn]    DATETIME         NULL,
     [ModifiedBy]    NVARCHAR (50)    NULL,
     [ts]            ROWVERSION       NOT NULL,
-    CONSTRAINT [PK_UserAccomplishments] PRIMARY KEY CLUSTERED ([UserID] ASC, [ApplicationID] ASC, [Type] ASC, [SubType] ASC, [AssociatedID] ASC),
+    CONSTRAINT [PK_UserAccomplishments] PRIMARY KEY NONCLUSTERED ([ID] ASC),
     CONSTRAINT [FK_UserAccomplishments_Applications] FOREIGN KEY ([ApplicationID]) REFERENCES [Lookups].[Applications] ([ID]),
     CONSTRAINT [FK_UserAccomplishments_Users] FOREIGN KEY ([UserID]) REFERENCES [Security].[Users] ([ID])
 );
+
+GO
+CREATE CLUSTERED INDEX [CI_UserAccomplishments]
+    ON [Security].[UserAccomplishments]([UserID] ASC, [ApplicationID] ASC, [Type] ASC, [SubType] ASC, [AssociatedID] ASC);
